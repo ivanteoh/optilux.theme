9243aec6325908a08d05d2a58e6a9ac046e52f88
Ivan Teoh <ivan@pretaweb.com>
2012-03-26 14:10:03 +1100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.0'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='optilux.theme',
+      version=version,
+      description="Theme for the Optilux website",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['optilux'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={'test': ['plone.app.testing']},
+      entry_points="""
+      # -*- Entry points: -*-
+  	  [z3c.autoinclude.plugin]
+  	  target = plone
+      """,
+      )
---
+++ src/optilux/theme/testing.py
@@ -0,0 +1,27 @@
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import applyProfile
+
+from zope.configuration import xmlconfig
+
+class OptiluxTheme(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE, )
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML for this package
+        import optilux.theme
+        xmlconfig.file('configure.zcml',
+                       optilux.theme,
+                       context=configurationContext)
+
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'optilux.theme:default')
+
+OPTILUX_THEME_FIXTURE = OptiluxTheme()
+OPTILUX_THEME_INTEGRATION_TESTING = \
+    IntegrationTesting(bases=(OPTILUX_THEME_FIXTURE, ),
+                       name="OptiluxTheme:Integration")
